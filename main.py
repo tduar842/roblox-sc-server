72b553bb7d165c1d3d5fa797f25fb31ca2490dee
tduar842 <159294094+tduar842@users.noreply.github.com>
2025-07-23 20:40:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PIL import Image
 import json
 import requests
 import time
+import math as m
 
 url = "https://roblox-sc-server.onrender.com/data"
 url_input = "https://roblox-sc-server.onrender.com/remote_inp"
@@ -11,9 +12,9 @@ headers = {
     "Content-Type": "application/json"
 }
 
-scale = 56
+scale = 58
 
-wait_time = 1
+wait_time = 0.5
 
 def recieve_input():
     response = requests.get(url=url_input)
@@ -26,6 +27,19 @@ def recieve_input():
         if data["action"] == "Mouse2":
             pyautogui.rightClick(data["x"], data["y"])
 
+        if data["action"] == "ArrowUp":
+            pyautogui.press("up")
+            
+        if data["action"] == "ArrowDown":
+            pyautogui.press("down")
+
+        if data["action"] == "ArrowLeft":
+            pyautogui.press("left")
+            
+        if data["action"] == "ArrowRight":
+            pyautogui.press("right")
+                                                
+
 def send_data(json_data):
     response = requests.post(url=url, json=json_data)
     print("status code: ", response.status_code)
@@ -37,13 +51,14 @@ def generate_screen_data(screenshot):
         for y in range(scale):
             r, g, b = screenshot.getpixel((x, y))
             data = {
-                "r" : r,
-                "g" : g,
-                "b" : b
+                "r" : m.floor((r/255)*100),
+                "g" : m.floor((g/255)*100),
+                "b" : m.floor((b/255)*100)
             }
             final_data.append(data)
+    #print(json.dumps(final_data))
+
     send_data(final_data)
-    #print(json_data)
 
 while True:
     time.sleep(wait_time)
